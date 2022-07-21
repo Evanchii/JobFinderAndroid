c5b91ebe7eb90a35f46fd6b1ac9814c6fa0ff3ff
Al Evan Castillo <castillo.alevan143@gmail.com>
2022-07-21 10:01:07 +0800

    ApplicantJobList +Added RV Adapter +Added Search Function
    *Needs onclick for RV Adapter

--- app/src/main/java/com/jobfinder/jobfinderandroid/ApplicantJobList.java
+++ app/src/main/java/com/jobfinder/jobfinderandroid/ApplicantJobList.java
@@ -4,19 +4,34 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
 
 public class ApplicantJobList extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private ActionBarDrawerToggle actionBarDrawerToggle;
+    private HashMap<String, HashMap<String, String>> jobList;
+    private DatabaseReference dbJobs;
+    private String search = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +40,8 @@ public class ApplicantJobList extends AppCompatActivity implements NavigationVie
         getSupportActionBar().setTitle("Find A Job");
         getSupportActionBar().setSubtitle("Applicant JobFinder");
         setContentView(R.layout.activity_applicant_job_list);
+
+        dbJobs = FirebaseDatabase.getInstance().getReference("jobs");
 
         DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerButton);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
@@ -35,6 +52,29 @@ public class ApplicantJobList extends AppCompatActivity implements NavigationVie
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.getMenu().getItem(1).setChecked(true);
+
+        getData();
+
+        SearchView searchView = findViewById(R.id.appJobList_search);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                search = s;
+                Log.d("AppJobList(62)", search);
+                getData();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if(s.equals("")) {
+                    search = s;
+                    Log.d("AppJobList(62)", search);
+                    getData();
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -62,5 +102,49 @@ public class ApplicantJobList extends AppCompatActivity implements NavigationVie
         // If you don't have res/menu, just create a directory named "menu" inside res
         getMenuInflater().inflate(R.menu.notification, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void getData() {
+        jobList = new HashMap<>();
+
+        dbJobs.get().addOnCompleteListener(task -> {
+            if(task.isComplete() && task.isComplete()) {
+                for(DataSnapshot data : task.getResult().getChildren()) {
+                    if(!search.equals("")) {
+                        if(data.child("companyName").getValue().toString().contains(search) ||
+                                data.child("jobTitle").getValue().toString().contains(search)) {
+                            HashMap<String, String> tmp = new HashMap<>();
+                            tmp.put("companyName", data.child("companyName").getValue().toString());
+                            tmp.put("jobTitle", data.child("jobTitle").getValue().toString());
+                            jobList.put(data.getKey(), tmp);
+                        }
+                    } else {
+                        HashMap<String, String> tmp = new HashMap<>();
+                        tmp.put("companyName", data.child("companyName").getValue().toString());
+                        tmp.put("jobTitle", data.child("jobTitle").getValue().toString());
+                        jobList.put(data.getKey(), tmp);
+                    }
+                }
+
+                inflateData();
+            }
+        });
+    }
+
+    public void inflateData() {
+        RecyclerView rv = findViewById(R.id.appJobList_rv);
+        if(!jobList.isEmpty()) {
+//            TextView empty = findViewById(R.id.txt_notifsEmpty);
+//            empty.setVisibility(View.GONE);
+            rv.setVisibility(View.VISIBLE);
+            rv.setLayoutManager(new LinearLayoutManager(this));
+            JobListAdapter adapter = new JobListAdapter(this, jobList);
+            rv.setAdapter(adapter);
+        } else {
+//            TextView empty = findViewById(R.id.txt_notifsEmpty);
+//            empty.setVisibility(View.VISIBLE);
+
+            rv.setVisibility(View.GONE);
+        }
     }
 }
---
+++ app/src/main/java/com/jobfinder/jobfinderandroid/JobListAdapter.java
@@ -0,0 +1,76 @@
+package com.jobfinder.jobfinderandroid;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+
+public class JobListAdapter extends RecyclerView.Adapter<JobListAdapter.ViewHolder> {
+
+    private HashMap<String, HashMap<String, String>> mData;
+    private LayoutInflater mInflater;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView companyName, jobTitle;
+        private ImageView logo;
+
+        public ViewHolder(View view) {
+            super(view);
+            companyName = view.findViewById(R.id.itemJob_txtCompany);
+            jobTitle = view.findViewById(R.id.itemJob_txtTitle);
+            logo = view.findViewById(R.id.itemJob_jobIcon);
+        }
+    }
+
+    public JobListAdapter(Context context, HashMap<String, HashMap<String, String>> mData) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = mData;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_job_list, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        String key = (String) (mData.keySet().toArray())[position];
+        Log.d("JLAdap(48)", key);
+        Log.d("JLAdap(49)", (String) (mData.get(key).keySet().toArray())[0]);
+
+        viewHolder.jobTitle.setText(mData.get(key).get("jobTitle"));
+        viewHolder.companyName.setText(mData.get(key).get("companyName"));
+
+        URL url = null;
+        try {
+            url = new URL("http://192.168.1.4/uploads/jobs/"+key+".png");
+            Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+
+            viewHolder.logo.setImageBitmap(bmp);
+        } catch (IOException e) {
+            e.printStackTrace();
+            viewHolder.logo.setImageResource(R.drawable.ic_briefcase);
+        }
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+}
